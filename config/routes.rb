6aa78b35d34bfbd3568d355c0fa91b34bf3e5f6d
Mccaryr <robert.mccary@yahoo.com>
2019-04-09 17:49:43 -0700

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
     get '/signin', to: 'sessions#new'
     post '/signin', to: 'sessions#create'
-    get '/logout', to: 'sessions#destroy' 
+    post '/logout', to: 'sessions#destroy' 
  
 end 

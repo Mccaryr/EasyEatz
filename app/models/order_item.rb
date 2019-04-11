7c472fd2e6d30d30c269c2715150a86fc71a1278
Mccaryr <robert.mccary@yahoo.com>
2019-04-11 10:04:22 -0700

    Updated Cart interface

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -19,9 +19,9 @@ before_save :finalize
 
     private 
 
-        def finalize 
-            self[:unit_price] = unit_price 
-            self[:total_price] = quantity * self[:unit_price]
-        end 
+    def finalize 
+      self[:unit_price] = unit_price 
+      self[:total_price] = quantity * self[:unit_price]
+    end 
 
 end 

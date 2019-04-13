73ec193751555e3fb60cc66ad706c68b4dbf1022
Mccaryr <robert.mccary@yahoo.com>
2019-04-13 13:22:47 -0700

    updated login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
     helper_method :current_order 
 
     def current_order
-        binding.pry
         if !session[:order_id].nil? 
             Order.find(session[:order_id])
         else 
